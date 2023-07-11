9d4489f7a86e838b6b475e776e5c1f99359fbee6
zhiyan114 <zhiyan114@zhiyan114.com>
2023-07-11 19:51:07 -0400

    Fix game15 compile error

--- applications/external/game15/game15.c
+++ applications/external/game15/game15.c
@@ -433,6 +433,8 @@ static void game_event_handler(GameEvent const event) {
                     sandbox_loop_exit();
                 }
                 break;
+            default:
+                break;
             }
         }
     } else if(event.type == EventTypeTick) {
